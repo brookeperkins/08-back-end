942bb354a28ab008341431a61ae80da275e3c7e1
brookeperkins <brookemmperkins@gmail.com>
2020-08-17 11:17:06 -0700

    images

--- data/queens.js
+++ data/queens.js
@@ -4,41 +4,47 @@ module.exports = [
     name: 'Ongina',
     winner: false,
     miss_congeniality: false,
-    quote: 'If I don\'t win this, I swear to god I\'m gonna cut someone!'
+    quote: 'If I don\'t win this, I swear to god I\'m gonna cut someone!',
+    image_url: 'http://www.nokeynoshade.party/images/ongina.jpg'
   },
   {
     id: 12,
     name: 'Bebe Zahara Benet',
     winner: true,
     miss_congeniality: false,
-    quote: 'I bring a lot of international influence, and right now I really feel like I\'m ahead of the game.'
+    quote: 'I bring a lot of international influence, and right now I really feel like I\'m ahead of the game.',
+    image_url: 'http://www.nokeynoshade.party/images/bebe-zahara-benet.jpg'
   },
   {
     id: 46,
     name: 'Latrice Royale',
     winner: false,
     miss_congeniality: true,
-    quote: 'Jesus is a biscuit! Let him sop you up!'
+    quote: 'Jesus is a biscuit! Let him sop you up!',
+    image_url: 'http://www.nokeynoshade.party/images/latrice-royale.jpg'
   },
   {
     id: 50,
     name: 'Penny Tration',
     winner: false,
     miss_congeniality: false,
-    quote: 'Hello boys!'
+    quote: 'Hello boys!',
+    image_url: 'http://www.nokeynoshade.party/images/penny-tration.jpg'
   },
   {
     id: 148,
     name: 'Nina West',
     winner: false,
     miss_congeniality: true,
-    quote: 'Welcome to Season 11. It looks like things just got... elevengendary.'
+    quote: 'Welcome to Season 11. It looks like things just got... elevengendary.',
+    image_url: 'https://www.nokeynoshade.party/images/nina-west.jpg',
   },
   {
     id: 126,
     name: 'Aquaria',
     winner: true,
     miss_congeniality: false,
-    quote: 'I don\'t define drag, drag is defined by me.'
+    quote: 'I don\'t define drag, drag is defined by me.',
+    image_url: 'http://www.nokeynoshade.party/images/aquaria.jpg',
   },
 ];
